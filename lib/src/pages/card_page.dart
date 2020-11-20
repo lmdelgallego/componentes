15b7846934ae26e6bd384b74b403d67450de992b
Luis Miguel Del Gallego <alucardluis@gmail.com>
2020-11-20 09:15:46 -0500

    Basic card

---
+++ lib/src/pages/card_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class CardPage extends StatelessWidget {
+  const CardPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Card'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(10),
+        children: [_cardTipo1()],
+      ),
+    );
+  }
+
+  Widget _cardTipo1() {
+    return Card(
+      child: Column(
+        children: [
+          ListTile(
+            leading: Icon(Icons.photo_album, color: Colors.blue),
+            title: Text('Soy el titulo de esta tarjeta'),
+            subtitle: Text(
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin laoreet magna enim, id lobortis nibh tincidunt sed. Nullam eget ex lectus. '),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              FlatButton(onPressed: () {}, child: Text('Cancelar')),
+              FlatButton(onPressed: () {}, child: Text('Ok')),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/src/routes/main_routes.dart
+++ lib/src/routes/main_routes.dart
@@ -1,5 +1,6 @@
 import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/pages/avatar_page.dart';
+import 'package:componentes/src/pages/card_page.dart';
 import 'package:componentes/src/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
@@ -8,5 +9,6 @@ Map<String, WidgetBuilder> getAplicationMainRoutes() {
     '/': (BuildContext contex) => HomePage(),
     'alert': (BuildContext contex) => AlertPage(),
     'avatar': (BuildContext contex) => AvatarPage(),
+    'card': (BuildContext contex) => CardPage(),
   };
 }
