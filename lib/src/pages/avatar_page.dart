5f169cc5e7b857e305d328f8e0d8f2ba9a3a9054
Luis Miguel Del Gallego <alucardluis@gmail.com>
2020-11-21 17:20:13 -0500

    Widget - Circle Avatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -8,6 +8,30 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            margin: EdgeInsets.only(right: 10),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://pley.today/__export/1582802383418/sites/mui/img/2020/02/22/scarlett-johansson-ensena-su-celulitis.png_691115875.png'),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10),
+            child: CircleAvatar(
+              child: Text('SJ'),
+              backgroundColor: Colors.brown,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage(
+              'https://media.vanityfair.com/photos/5ddd5c759aeeef0008170fc0/16:9/w_1280,c_limit/a-scarlett-johansson-oscars-tout.jpg'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
       ),
     );
   }
