55a99b7b0afafe33debd91a80ea410c75d645a2b
Luis Miguel Del Gallego <alucardluis@gmail.com>
2020-11-21 17:51:23 -0500

    Normal Input

---
+++ lib/src/pages/input_pate.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  InputPage({Key key}) : super(key: key);
+
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  String _nombre = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Inputs de texto'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+        children: [
+          _crearInput(),
+          Divider(),
+          _crearPersona(),
+        ],
+      ),
+    );
+  }
+
+  Widget _crearInput() {
+    return TextField(
+      autofocus: false,
+      textCapitalization: TextCapitalization.sentences,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+        counter: Text('Letras ${_nombre.length}'),
+        hintText: 'Nombre de la persona',
+        labelText: 'Nombre',
+        helperText: 'Solo es el nombre',
+        suffixIcon: Icon(Icons.accessibility),
+        icon: Icon(Icons.account_circle),
+      ),
+      onChanged: (value) {
+        setState(() {
+          _nombre = value;
+        });
+      },
+    );
+  }
+
+  Widget _crearPersona() {
+    return ListTile(
+      title: Text('Nombre es : $_nombre'),
+    );
+  }
+}
--- lib/src/routes/main_routes.dart
+++ lib/src/routes/main_routes.dart
@@ -3,6 +3,7 @@ import 'package:componentes/src/pages/animated_container.dart';
 import 'package:componentes/src/pages/avatar_page.dart';
 import 'package:componentes/src/pages/card_page.dart';
 import 'package:componentes/src/pages/home_page.dart';
+import 'package:componentes/src/pages/input_pate.dart';
 import 'package:flutter/material.dart';
 
 Map<String, WidgetBuilder> getAplicationMainRoutes() {
@@ -12,5 +13,6 @@ Map<String, WidgetBuilder> getAplicationMainRoutes() {
     'avatar': (BuildContext contex) => AvatarPage(),
     'card': (BuildContext contex) => CardPage(),
     'animatedContainer': (BuildContext contex) => AnimatedContainerPage(),
+    'inputs': (BuildContext contex) => InputPage(),
   };
 }
