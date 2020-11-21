2ee02236aff7243604daef9b8cbe177a3b00b52b
Luis Miguel Del Gallego <alucardluis@gmail.com>
2020-11-21 18:18:05 -0500

    Cambiar idiomas

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'package:componentes/src/routes/main_routes.dart';
 import 'package:componentes/src/pages/alert_page.dart';
@@ -9,8 +10,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Cupertino App',
-      // debugShowCheckedModeBanner: false,
+      title: 'Componentes App',
+      debugShowCheckedModeBanner: false,
+      localizationsDelegates: [
+        // ... app-specific localization delegate[s] here
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', ''), // English, no country code
+        const Locale('es', ''), // Arabic, no country code
+      ],
       // home: HomePage(),
       initialRoute: '/',
       routes: getAplicationMainRoutes(),
